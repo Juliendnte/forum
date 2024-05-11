478f0a3cee74f2f86f6bab83524d861d1db647a2
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-05-11 11:15:45 +0200

    Closes #12

--- back/api/models/userModel.js
+++ back/api/models/userModel.js
@@ -10,12 +10,37 @@ class UserModel{
         });
     }
 
-    static getAllUser(){
+    static getAllUser(query){
         return new Promise((resolve, reject) => {
-            const sql = `SELECT * FROM users;`
-            connection.query(sql, (err, results)=> {
-                err ? reject(err) : resolve(results);
-            });
+            let sql = `SELECT * FROM users `
+
+            //S'il y a quelque chose dans la query
+            if (!(Object.entries(query).length === 0)){
+                const values = [];
+
+                /*
+                Un objet marche comme une map en clé valeur
+                Name: Julien
+                la clé est Name et la valeur Julien
+                Ici je met la clé dans la requête sql, la valeur dans une liste qui remplacera les ?
+                Et j'ai un index pour mettre un WHERE si il y a une query autre que limit ou offset
+                Ma fonction ne marche que si le limit et offset sont a la fin comme en sql
+                 */
+                Object.entries(query).forEach(([key, value], index) => {
+                    if (key.toLowerCase() === "limit" || key.toLowerCase() === "offset"){
+                        sql += `${key} ? `;
+                        values.push(parseInt(value));
+                    }else{
+                        sql += index ===0 ? "WHERE ": "AND "
+                        sql += `${key}=? `;
+                        values.push(value);
+                    }
+                });
+
+                connection.query(sql,values, (err, results)=> err ? reject(err) : resolve(results));
+            }else{
+                connection.query(sql, (err, results)=> err ? reject(err) : resolve(results));
+            }
         });
     }
 
