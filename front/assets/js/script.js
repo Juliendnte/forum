69c57a09cbe28c85a69642eded53ff9f9ff2b371
KANTIN-FAGN <kantin.fagniart@ynov.com>
2024-05-16 09:43:04 +0200

    Closes #19

---
+++ front/assets/js/script.js
@@ -0,0 +1,14 @@
+// Sélectionner tous les éléments de sous-menu
+const subMenus = document.querySelectorAll('.sousMenu');
+
+// Parcourir tous les éléments de sous-menu et ajouter un gestionnaire d'événement de clic
+subMenus.forEach(subMenu => {
+    subMenu.previousElementSibling.addEventListener('click', (event) => {
+        // Empêcher le comportement par défaut du lien
+        event.preventDefault();
+        // Basculer la classe active pour activer ou désactiver l'animation
+        subMenu.nextElementSibling.classList.toggle('active');
+        // Tourner le SVG
+        event.currentTarget.querySelector('.arrow-svg').classList.toggle('rotate');
+    });
+});
