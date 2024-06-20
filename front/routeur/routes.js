eae579de91b4184a263b609e0ea843f92b613bc3
KAVTIV <140246837+KANTIN-FAGN@users.noreply.github.com>
2024-06-20 18:28:12 +0200

    Closes #36

--- front/routeur/routes.js
+++ front/routeur/routes.js
@@ -1,18 +1,21 @@
 //Importation des modules
 const express = require("express");
 const routeur = express.Router();
-const controller = require("../controller/control");
+const controllerUser = require("../controller/controlTreatmentUser");
+const controllerPage = require("../controller/controlPage");
 
 //Configuration des routes
-routeur.get("/CODER", controller.Index);
-routeur.get("/CODER/login", controller.Login)
-routeur.get("/CODER/register", controller.Register)
-routeur.get("/CODER/mot_de_passe_oublie", controller.ForgotPwd)
-routeur.get("/CODER/profil", controller.ProfilUser)
+routeur.get("/CODER", controllerPage.Index);
+routeur.get("/CODER/login", controllerPage.Login)
+routeur.get("/CODER/register", controllerPage.Register)
+routeur.get("/CODER/accounts/password/reset/", controllerPage.ForgotPwd)
+routeur.get("/CODER/profil", controllerPage.ProfilUser)
 
-routeur.post("/treatment/login", controller.LoginTreatment)
-routeur.post("/treatment/register" , controller.RegisterTreatment)
-routeur.post("/treatment/forgotpwd" , controller.RegisterTreatment)
+// Route Traitement en rapport avec le user
+routeur.post("/treatment/login", controllerUser.LoginTreatment)
+routeur.post("/treatment/register", controllerUser.RegisterTreatment)
+routeur.post("/treatment/forgotpwd", controllerUser.ForgotPwdTreatment)
+routeur.post("/treatment/followUser", controllerUser.FollowUserTreatment)
 
 //pour que nos routes soit accessible
 module.exports = routeur
