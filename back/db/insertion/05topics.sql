763e87b6cd2eb70b5d24430c1ed030170ac78cb6
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-06-27 22:28:36 +0200

    Closes #77

--- back/db/insertion/05topics.sql
+++ back/db/insertion/05topics.sql
@@ -1,3 +1,3 @@
-INSERT INTO `topics` (Title, Id_Status, Id_User)
-VALUES ('carrouselsJs', 1, 1),
-       ('responsive', 1, 1);
+INSERT INTO `topics` (Title, Id_Status, Id_User, Create_at)
+VALUES ('carrouselsJs', 1, 1, '2024-04-01'),
+       ('responsive', 1, 1, '2024-04-02');
