8dd9934c0bc4122690ceaf28922b8eb3dd0b5b88
KAVTIV <140246837+KANTIN-FAGN@users.noreply.github.com>
2024-06-30 12:48:17 +0200

    Closes #104

--- front/assets/js/scriptUpdateProfil.js
+++ front/assets/js/scriptUpdateProfil.js
@@ -55,5 +55,15 @@ function submitForm() {
     document.getElementById('tags-form').submit();
 }
 
+document.getElementById('profileImageInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profileImagePreview').src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
 
 
