2417b6b8696858168ede121f139c0743beea3393
KAVTIV <140246837+KANTIN-FAGN@users.noreply.github.com>
2024-07-04 02:07:07 +0200

    Closes #141

--- front/assets/js/scriptSearchFriendsFollow.js
+++ front/assets/js/scriptSearchFriendsFollow.js
@@ -19,3 +19,24 @@ document.getElementById('searchFriend').addEventListener('input', function() {
         document.querySelector('.message-friend-not-found').style.display = 'none';
     }
 });
+
+document.getElementById('searchFollower').addEventListener('input', function() {
+    let filter = this.value.toLowerCase();
+    let followers = document.querySelectorAll('.UserFriend-item');
+
+    followers.forEach(follower => {
+        let name = follower.getAttribute('data-name');
+        if (name.startsWith(filter)) {
+            follower.style.display = '';
+        } else {
+            follower.style.display = 'none';
+        }
+    });
+
+    let visibleFollowers = Array.from(followers).filter(follower => follower.style.display !== 'none');
+    if (visibleFollowers.length === 0) {
+        document.querySelector('.message-friend-not-found').style.display = 'block';
+    } else {
+        document.querySelector('.message-friend-not-found').style.display = 'none';
+    }
+});
